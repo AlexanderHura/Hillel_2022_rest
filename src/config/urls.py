39e98b823a1fba9a733de36a15be30d50c7aeb3e
AlexanderHura <alexander_gura@ukr.net>
2022-12-25 19:42:30 +0200

    add black

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,20 +1,17 @@
-
-from django.http import JsonResponse
 from django.contrib import admin
-from django.urls import path, include
+from django.http import JsonResponse
+from django.urls import include, path
+
 
 def check(request):
-    #return HttpResponse("Hello")
+    # return HttpResponse("Hello")
     data = {"name": "Dima"}
-    #return HttpResponse(str(data), content_type="application/json")
+    # return HttpResponse(str(data), content_type="application/json")
     return JsonResponse(data)
-    
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('health-check/', check),
-    path('posts/', include('posts.urls')),
- 
-
-
+    path("admin/", admin.site.urls),
+    path("health-check/", check),
+    path("posts/", include("posts.urls")),
 ]
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,9 +1,9 @@
-
 from django.urls import path
+
 from .api import posts
 
 app_name = "posts"
 
 urlpatterns = [
     path("", posts, name="list"),
-   ]
+]
